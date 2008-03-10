a16902c07e86635a9f334411ea80012ece584762
Holger Freyther <zecke@selfish.org>
2008-03-10 11:56:06 +0100

    Sketch the interface of what we want to expose via DBus for an illume Gadget     This is a temporary interface and it will go away once we have     our DBus middleware layer fully specified.     Exporting enums via DBus is too easy, instead of exporting random integers     I have opted for strings.

---
+++ src/server/phone/phonestatusdbusexporter.h
@@ -0,0 +1,64 @@
+/***************************************************************************
+**
+** Copyright (C) 2008 Openmoko Inc.
+**
+** This software is licensed under the terms of the GNU General Public
+** License (GPL) version 2 or at your option any later version.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+***************************************************************************/
+
+#ifndef PHONESTATUSDBUSEXPORTER_H
+#define PHONESTATUSDBUSEXPORTER_H
+
+#ifdef QT_ILLUME_LAUNCHER
+
+#include <QDBusConnection>
+
+#include "cellmodemmanager.h"
+
+class SimPinDialog;
+class DefaultSignal;
+
+class PhoneStatusDBusExporter : public QObject {
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.openmoko.qtopia.Phonestatus")
+    Q_ENUMS(ModemState PlaneMode)
+    Q_PROPERTY(QString phoneState READ phoneState)
+    Q_PROPERTY(bool planeModeEnabled READ planeModeEnabled WRITE setPlaneModeEnabled)
+    Q_PROPERTY(int signalStrength READ signalStrength)
+
+public:
+    PhoneStatusDBusExporter(SimPinDialog* dialog, QObject* parent);
+
+Q_SIGNALS:
+    Q_SCRIPTABLE void stateChanged(QString);
+    Q_SCRIPTABLE void planeModeChanged(bool);
+    Q_SCRIPTABLE void signalStrengthChanged(int percent);
+
+public Q_SLOTS:
+    Q_SCRIPTABLE QString phoneState() const;
+    Q_SCRIPTABLE bool planeModeEnabled() const;
+    Q_SCRIPTABLE int signalStrength() const;
+
+    Q_SCRIPTABLE Q_NOREPLY void setPlaneModeEnabled(bool);
+    Q_SCRIPTABLE Q_NOREPLY void triggerPinDialog();
+
+private Q_SLOTS:
+    void _q_stateChanged(CellModemManager::State, CellModemManager::State);
+    void _q_planeModeEnabledChanged(bool);
+
+private:
+    static QString cellStatusToModemState(CellModemManager::State);
+
+private:
+    SimPinDialog *m_pinDialog;
+    CellModemManager *m_cellModem;
+    DefaultSignal *m_defaultSignal;
+};
+
+#endif
+
+#endif
