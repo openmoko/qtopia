0a6eb40b0e18becb41c637b5652fafde5903616b
Holger Freyther <zecke@selfish.org>
2008-03-10 17:54:05 +0100

    PhoneStatus: Export the network operator

--- src/server/phone/phonestatusdbusexporter.h
+++ src/server/phone/phonestatusdbusexporter.h
@@ -28,6 +28,7 @@ class PhoneStatusDBusExporter : public QObject {
     Q_PROPERTY(QString phoneState READ phoneState)
     Q_PROPERTY(bool planeModeEnabled READ planeModeEnabled WRITE setPlaneModeEnabled)
     Q_PROPERTY(int signalStrength READ signalStrength)
+    Q_PROPERTY(QString networkOperator READ networkOperator)
 
 public:
     PhoneStatusDBusExporter(SimPinDialog* dialog, QObject* parent);
@@ -36,11 +37,13 @@ Q_SIGNALS:
     Q_SCRIPTABLE void stateChanged(QString);
     Q_SCRIPTABLE void planeModeChanged(bool);
     Q_SCRIPTABLE void signalStrengthChanged(int percent);
+    Q_SCRIPTABLE void networkOperatorChanged(QString);
 
 public Q_SLOTS:
     Q_SCRIPTABLE QString phoneState() const;
     Q_SCRIPTABLE bool planeModeEnabled() const;
     Q_SCRIPTABLE int signalStrength() const;
+    Q_SCRIPTABLE QString networkOperator() const;
 
     Q_SCRIPTABLE Q_NOREPLY void setPlaneModeEnabled(bool);
     Q_SCRIPTABLE Q_NOREPLY void triggerPinDialog();
