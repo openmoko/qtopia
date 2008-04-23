3019cc28cb1c5bc33acc61c9433cb40a6f930963
Holger Freyther <zecke@selfish.org>
2008-04-23 16:33:56 +0200

    [prediction] First implementation of the C Interface, untested

--- src/libraries/qtopiaprediction/wordprediction.cpp
+++ src/libraries/qtopiaprediction/wordprediction.cpp
@@ -13,3 +13,77 @@
 #include "wordprediction.h"
 
 #include "pred.h"
+
+#define TO_PREDICT_STATE(handle) reinterpret_cast<PredictState*>(handle)
+#define TO_WORD_PREDICT(handle)  reinterpret_cast<WordPredict*>(TO_PREDICT_STATE(handle))
+
+struct PredictState {
+    PredictState(const WordPredict::Config& config, int max_words)
+        : predict(config, max_words)
+    {}
+
+    WordPredict predict;
+    QString currentWord;
+};
+
+extern "C" {
+word_prediction_handle word_prediction_init(int max_words, int swidth)
+{
+    WordPredict::Config config;
+    config.reallyNoMoveSensitivity = swidth / 48;
+    config.moveSensitivity = swidth / 12;
+    config.excludeDistance = (swidth * 10) / 48;
+    return new PredictState(config, max_words);
+}
+
+void word_prediction_free(word_prediction_handle handle)
+{
+    delete TO_PREDICT_STATE(handle);
+}
+
+void word_prediction_set_position(word_prediction_handle handle, char key, int x, int y, int width, int height)
+{
+    // TODO: use width and height
+    TO_WORD_PREDICT(handle)->setLetter(key, QPoint(x, y));
+}
+
+void word_prediction_add_perfect_letter(word_prediction_handle handle, char letter)
+{
+    TO_WORD_PREDICT(handle)->addLetter(letter);
+}
+
+void word_prediction_add_touch(word_prediction_handle handle, int x, int y)
+{
+    TO_WORD_PREDICT(handle)->addTouch(QPoint(x, y));
+}
+
+void  word_prediction_clear_word(word_prediction_handle handle)
+{
+    TO_WORD_PREDICT(handle)->reset();
+}
+
+void  word_prediction_get_number_words(word_prediction_handle handle)
+{
+    TO_WORD_PREDICT(handle)->words().count();
+}
+
+char* word_prediction_get_word(word_prediction_handle handle, int index)
+{
+    PredictState* state = TO_PREDICT_STATE(handle);
+    state->currentWord = state->predict.words()[index];
+    return state->currentWord.toUtf8().data();
+}
+
+float word_prediction_get_rate(word_prediction_handle handle, int index)
+{
+    return TO_WORD_PREDICT(handle)->ratings()[index];
+}
+
+char* word_prediction_get_best_word(word_prediction_handle handle)
+{
+    PredictState* state = TO_PREDICT_STATE(handle);
+    state->currentWord = state->predict.word();
+    return state->currentWord.toUtf8().data();
+}
+
+}
--- src/libraries/qtopiaprediction/wordprediction.h
+++ src/libraries/qtopiaprediction/wordprediction.h
@@ -15,13 +15,6 @@
 
 extern "C" {
 
-#define MOVEMENT_NONE                   0x0
-#define MOVEMENT_UP                     0x1
-#define MOVEMENT_DOWN                   0x2
-#define MOVEMENT_LEFT                   0x4
-#define MOVEMENT_RIGHT                  0x8
-#define MOVEMENT_PERFECT                0x10
-
 /**
  * Handle to the internal data structure
  */
@@ -31,40 +24,45 @@ typedef void* word_prediction_handle;
  * Create and initialize a handle, specify some
  * configuration.
  */
-word_prediction_handle* word_prediction_init();
-void word_prediction_free(word_prediction_handle* handle);
+word_prediction_handle word_prediction_init(int max_words, int width);
+void word_prediction_free(word_prediction_handle handle);
 
 /**
  * Where is the center of the @param key? This must be properly initialized for
  * every key used by the keyboard layout.
  */
-void word_prediction_set_center(word_prediction_handle* handle, char key, int x, int y);
+void word_prediction_set_position(word_prediction_handle handle, char key, int x, int y, int width, int height);
 
 /**
  * Add the letter to the prediction a perfect hit at the position of @param letter
  * is assumed.
  */
-void word_prediction_add_perfect_letter(word_prediction_handle* handle, char letter);
+void word_prediction_add_perfect_letter(word_prediction_handle handle, char letter);
 
 /**
  * Add a touch and describe the movement from the previos format. The movement has to
- * be NONE for the first charachter
+ * be NONE for the first charachter.
  */
-void word_prediction_add_touch(word_prediction_handle* handle, int x, int y, int movement);
+void word_prediction_add_touch(word_prediction_handle handle, int x, int y);
 
 /**
  * Call this to start a new word.
  */
-void  word_prediction_clear_word(word_prediction_handle* handle);
+void  word_prediction_clear_word(word_prediction_handle handle);
 
 /**
  * Get the number of predicted words. This can be used to iterate over
  * the list of words and their ratings.
  */
-void  word_prediction_get_number_words(word_prediction_handle*);
-char* word_prediction_get_word(word_prediction_handle*, int index);
-int   word_prediction_get_rate(word_prediction_handle*, int index);
-char* word_prediction_get_best_word(word_prediction_handle*);
+void  word_prediction_get_number_words(word_prediction_handle handle);
+
+/**
+ * Returns the word in UTF-8. The pointer is valid until the next call to
+ * get_best_word, get_word or _reset.
+ */
+char* word_prediction_get_word(word_prediction_handle handle, int index);
+float word_prediction_get_rate(word_prediction_handle handle, int index);
+char* word_prediction_get_best_word(word_prediction_handle handle);
 
 
 
