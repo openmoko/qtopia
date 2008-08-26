openmoko/qtopia

commit 9971aa8a69e31057b0f02af43b4c20333a77f13e

[ficgta01] Fix the No registration bug (#1766)     In 8380a8534e802c152c160e6699634ad9c66b1951 the SIM initialisation was     postponed until after receiving "%CSTAT RDY, 1" from the modem but     according to https://docs.openmoko.org/trac/ticket/1766 for some people     RDY is never sent.
    Postpone initialisation until after SMS and PHB are ready (or if RDY
    comes first use that). That should resolve #1766 and still delay the init
    enough to have listing of the phonebook working.

    The code has an early exit for the case that status is not 1 and for the
    %CSTAT EONS, 1 it might fail but we are not interested in that notification
    anyway and it is doing no harm.

    This is minimally tested.

devices/ficgta01/src/plugins/phonevendors/ficgta01/vendor_ficgta01.cpp
@@ -411,6 +411,8 @@ Ficgta01ModemService::Ficgta01ModemService
     : QModemService( service, mux, parent )
     , m_vibratorService( 0 )
     , m_phoneBook( 0 )
+    , m_phoneBookIsReady( false )
+    , m_smsIsReady( false )
 {
     connect( this, SIGNAL(resetModem()), this, SLOT(reset()) );
 
@@ -650,15 +652,30 @@ void Ficgta01ModemService::cstatNotification( const QString& msg )
     // SMS
     // RDY (Ready when both PHB and SMS have reported they are ready)
     QString entity = msg.mid(8, 3);
+    uint status = msg.mid(13).toInt();
 
-    if (entity == "RDY" ) {
-        uint status = msg.mid(13).toInt();
-        if (status == 1) {
-            post("simready"); 
+    // Something is not ready, ignore
+    if (status != 1)
+        return;
 
-            if (m_phoneBook)
-                m_phoneBook->sendPhoneBooksReady();
-        }
+    // We are already ready, ignore
+    if (m_phoneBookIsReady && m_smsIsReady)
+        return;
+
+    if (entity == "PHB")
+        m_phoneBookIsReady = true;
+    else if (entity == "SMS")
+        m_smsIsReady = true;
+    else if (entity == "RDY") {
+        m_smsIsReady = true;
+        m_phoneBookIsReady = true;
+    }
+
+    if  (m_smsIsReady && m_phoneBookIsReady) {
+        post("simready"); 
+
+        if (m_phoneBook)
+            m_phoneBook->sendPhoneBooksReady();
     }
 }
 

devices/ficgta01/src/plugins/phonevendors/ficgta01/vendor_ficgta01_p.h
@@ -145,6 +145,9 @@ private slots:
 private:
     Ficgta01VibrateAccessory* m_vibratorService;
     Ficgta01PhoneBook* m_phoneBook;
+
+    bool m_phoneBookIsReady;
+    bool m_smsIsReady;
 };
 
 class  Ficgta01VibrateAccessory : public QVibrateAccessoryProvider

commit 13101ab6ddaec380871ae8021a92140af526c4df

[ficgta01] Fix phonebook init regression from 9971aa8a69e31057b0f02af43b4c20333a77f13e     When not waiting for RDY, 1 anymore my phone started to show the old     issue with not querying the addressbook correctly. For some reason somewhen     early in the init sequence a %CSTAT PHB, 1 comes by and we set     m_phoneBookIsReady to true.
    To fix this issue and hopefully not regress in another way we keep
    track of the status. So if phonebook goes from 0, 1 to 0 again we set
    m_phoneBookIsReady is to false. This means we wait for the point when both
    SMS and PHB are ready.

devices/ficgta01/src/plugins/phonevendors/ficgta01/vendor_ficgta01.cpp
@@ -654,21 +654,17 @@ void Ficgta01ModemService::cstatNotification( const QString& msg )
     QString entity = msg.mid(8, 3);
     uint status = msg.mid(13).toInt();
 
-    // Something is not ready, ignore
-    if (status != 1)
-        return;
-
     // We are already ready, ignore
     if (m_phoneBookIsReady && m_smsIsReady)
         return;
 
     if (entity == "PHB")
-        m_phoneBookIsReady = true;
+        m_phoneBookIsReady = status;
     else if (entity == "SMS")
-        m_smsIsReady = true;
+        m_smsIsReady = status;
     else if (entity == "RDY") {
-        m_smsIsReady = true;
-        m_phoneBookIsReady = true;
+        m_smsIsReady = status;
+        m_phoneBookIsReady = status;
     }
 
     if  (m_smsIsReady && m_phoneBookIsReady) {